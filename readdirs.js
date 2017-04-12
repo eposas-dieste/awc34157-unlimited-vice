2a242cb4a4dc1f40d5217a22a9da543a013958fa
eposasdieste <ericposas06@gmail.com>
2017-04-12 11:40:44 -0500

    edited readdirs.js utility

--- readdirs.js
+++ readdirs.js
@@ -1,6 +1,16 @@
-const testFolder = 'SPANISH/';
+const arg = process.argv[2];
 const fs = require('fs');
+var testFolder;
 var arr = [];
+
+if(arg === 's' || 'span' || 'spanish' || 'SPANISH'){
+  testFolder = 'SPANISH';
+}else if(arg === 'e' || 'eng' || 'english' || 'ENGLISH'){
+  testFolder = 'ENGLISH';
+}else{
+  testFolder = 'SPANISH';
+}
+
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
     arr.push(file);
@@ -10,3 +20,11 @@ fs.readdir(testFolder, (err, files) => {
   }
   console.log(arr);
 });
+
+/* prints all arguments passed to the node script 
+(function(){
+  process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+  });
+}());
+*/
